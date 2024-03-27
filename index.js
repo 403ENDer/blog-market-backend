9a1396b79be3880f975ca21f8104607b3da6489b
Harish Kumar N <harish11ndsh@gmail.com>
2024-03-27 10:54:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,8 +26,6 @@ app.use(
   cors({
     origin: ["https://blog-master1106.netlify.app", "http://localhost:5173"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
